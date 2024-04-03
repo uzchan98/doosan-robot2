4e604076fc4cd3996bc3bf9843efc206a6236a7e
Minsoo Song <sms0432@gmail.com>
2024-04-02 17:36:51 -0900

    add collision & launch params

---
+++ dsr_bringup2/launch/dsr_bringup2_gazebo.launch.py
@@ -0,0 +1,189 @@
+# 
+#  dsr_bringup2
+#  Author: Minsoo Song (minsoo.song@doosan.com)
+#  
+#  Copyright (c) 2024 Doosan Robotics
+#  Use of this source code is governed by the BSD, see LICENSE
+# 
+
+import os
+
+from launch import LaunchDescription
+from launch.actions import RegisterEventHandler,DeclareLaunchArgument
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from ament_index_python.packages import get_package_share_directory
+
+
+ARGUMENTS =[ 
+    DeclareLaunchArgument('name',  default_value = 'dsr01',     description = 'NAME_SPACE'     ),
+    DeclareLaunchArgument('host',  default_value = '192.168.137.100', description = 'ROBOT_IP'       ),
+    DeclareLaunchArgument('port',  default_value = '12345',     description = 'ROBOT_PORT'     ),
+    DeclareLaunchArgument('mode',  default_value = 'real',   description = 'OPERATION MODE' ),
+    DeclareLaunchArgument('model', default_value = 'm1013',     description = 'ROBOT_MODEL'    ),
+    DeclareLaunchArgument('color', default_value = 'white',     description = 'ROBOT_COLOR'    ),
+]
+
+def generate_launch_description():
+    
+    xacro_path = os.path.join( get_package_share_directory('dsr_description2'), 'xacro')
+    
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("dsr_description2"),
+                    "xacro",
+                    LaunchConfiguration('model'),
+                ]
+            ),
+            ".urdf.xacro",
+        ]
+    )
+
+    robot_description = {"robot_description": robot_description_content}
+
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare("dsr_controller2"),
+            "config",
+            "dsr_controller2.yaml",
+        ]
+    )
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("dsr_description2"), "rviz", "default.rviz"]
+    )
+    
+    connection_node = Node(
+        package="dsr_bringup2",
+        executable="connection",
+        parameters=[
+            {"name":    LaunchConfiguration('name')  }, 
+            {"rate":    100         },
+            {"standby": 5000        },
+            {"command": True        },
+            {"host":    LaunchConfiguration('host')  },
+            {"port":    LaunchConfiguration('port')  },
+            {"mode":    LaunchConfiguration('mode')  },
+            {"model":   LaunchConfiguration('model') },
+            {"gripper": "none"      },
+            {"mobile":  "none"      },
+            #parameters_file_path       # 파라미터 설정을 동일이름으로 launch 파일과 yaml 파일에서 할 경우 yaml 파일로 셋팅된다.    
+        ],
+        output="screen",
+    )
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, robot_controllers],
+        remappings=[
+            (
+                "/forward_position_controller/commands",
+                "/position_commands",
+            ),
+        ],
+        output="both",
+    )
+    # robot_state_pub_node = Node(
+    #     package="robot_state_publisher",
+    #     executable="robot_state_publisher",
+    #     output="both",
+    #     remappings=[
+    #         (
+    #             "/joint_states",
+    #             "/dsr/joint_states",
+    #         ),
+    #     ],
+    #     parameters=[robot_description],
+    # )
+    robot_state_pub_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='both',
+        parameters=[{
+        'robot_description': Command(['xacro', ' ', xacro_path, '/', LaunchConfiguration('model'), '.urdf.xacro color:=', LaunchConfiguration('color')])           
+    }])
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    robot_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["dsr_controller2", "-c", "/controller_manager"],
+    )
+    
+    joint_trajectory_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["dsr_joint_trajectory", "-c", "/controller_manager"],
+    )
+
+    # joint_state_publisher_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=["dsr_joint_publisher", "--controller-manager", "/controller_manager"],
+    # )
+    
+    # Delay connection start after registering user's param`
+    # delay_control_node_after_connection_node = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=connection_node,
+    #         on_exit=[control_node],
+    #     )
+    # )
+
+    # Delay rviz start after `joint_state_broadcaster`
+    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[rviz_node],
+        )
+    )
+
+    # Delay start of robot_controller after `joint_state_broadcaster`
+    delay_robot_controller_spawner_after_joint_state_broadcaster_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[robot_controller_spawner],
+        )
+    )
+
+    # Delay start of robot_controller after `joint_state_broadcaster`
+    delay_robot_controller_spawner_after_joint_trajectory_controller_spawner = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=joint_state_broadcaster_spawner,
+            on_exit=[joint_trajectory_controller_spawner],
+        )
+    )
+
+    nodes = [
+        connection_node,
+        control_node,
+        robot_state_pub_node,
+        joint_state_broadcaster_spawner,
+        # joint_state_publisher_spawner,
+        delay_rviz_after_joint_state_broadcaster_spawner,
+        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
+        # delay_robot_controller_spawner_after_joint_trajectory_controller_spawner,
+    ]
+
+    return LaunchDescription(ARGUMENTS + nodes)
