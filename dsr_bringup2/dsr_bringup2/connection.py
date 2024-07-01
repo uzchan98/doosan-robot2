5cb7b605e2b7e6116ba68e18def233c6459e2b13
Minju, Lee <minju3.lee@doosan.com>
2024-07-01 18:37:56 +0900

    Add: control lifetime of Virtual DRCF according to instance

--- dsr_bringup2/dsr_bringup2/connection.py
+++ dsr_bringup2/dsr_bringup2/connection.py
@@ -1,13 +1,37 @@
+import os
+import yaml
+import signal
+
+from ament_index_python.packages import get_package_share_directory
 import rclpy
 from rclpy.node import Node
-from rclpy.parameter import Parameter
 
-import os
-from pathlib import Path
-from ament_index_python.packages import get_package_share_directory
+class VirtualDRCF():
+    def __init__(self, port, model, name):
+        self.emulator_name = name + "_emulator"
+        self.run_drcf(port, model, name)
+        # signal.signal(signal.SIGINT, self.terminate_drcf)
+        # signal.signal(signal.SIGTERM, self.terminate_drcf)
+    
+    ## TODO(Improve) : The context seems to can't handle signal perfectly.
+    # needed to fix after https://github.com/ros2/rclpy/issues/1287
+    def __del__(self):
+        self.terminate_drcf()
 
-import yaml
+    def run_drcf(self, port, model, name):
+        run_script_path = os.path.join(
+            get_package_share_directory("common2"), "bin"
+        )
+        start_cmd = "{}/run_drcf.sh ".format(run_script_path) +" "+ str(port)+" "+ model +" " +name
+        os.system(start_cmd)
 
+    def terminate_drcf(self):
+        stop_cmd = "docker ps -a --filter name={} -q | xargs -r docker stop".\
+            format(self.emulator_name)
+        print("stop_cmd : ",stop_cmd)
+        os.system(stop_cmd)
+        rclpy.shutdown()
+        exit(1)
 
 class ConnectionNode(Node):
     def __init__(self):
@@ -20,13 +44,11 @@ class ConnectionNode(Node):
         self.declare_parameter('command', True)
         self.declare_parameter('host', '127.0.0.1')
         self.declare_parameter('port', 12345)
-        self.declare_parameter('mode', 'real')
+        self.declare_parameter('mode', 'virtual')
         self.declare_parameter('model', 'm1013')
         self.declare_parameter('gripper', 'none')
         self.declare_parameter('mobile', 'none')
-        
-        self.my_parameter_ = self.get_parameter('mobile').value
-        self.get_logger().info('mobile: %s' % self.my_parameter_)
+
         parameters = {}
         parameters['name'] = self.get_parameter('name').get_parameter_value().string_value
         parameters['rate'] = self.get_parameter('rate').get_parameter_value().integer_value
@@ -38,36 +60,26 @@ class ConnectionNode(Node):
         parameters['model'] = self.get_parameter('model').get_parameter_value().string_value
         parameters['gripper'] = self.get_parameter('gripper').get_parameter_value().string_value
         parameters['mobile'] = self.get_parameter('mobile').get_parameter_value().string_value
-        
+
         current_file_path = os.path.join(
             get_package_share_directory("dsr_hardware2"), "config"
         )
-        run_script_path = os.path.join(
-            get_package_share_directory("common2"), "bin"
-        )
         os.makedirs(current_file_path, exist_ok=True)
-        
-            # exec(command)
-            # print(command)
-            # eval("{}/run_drcf.sh ".format(run_script_path), host, str(port), name)
-        
-        with open(os.path.join(current_file_path, 'parameters.yaml'), 'w') as file:
+        param_name = self.get_namespace()[1:] +'_parameters.yaml'
+        with open(os.path.join(current_file_path, param_name), 'w') as file:
             yaml.dump(parameters, file)
+
+        ## Run Virtual DRCF if necessary.
         if parameters['mode'] == 'virtual':
-            port = parameters['port']
-            model = parameters['model']
-            name = parameters['name']
-            command = "{}/run_drcf.sh ".format(run_script_path) +" "+ str(port)+" "+ model +" " +name
-            # import os
+            port, model, name = parameters['port'], parameters['model'], parameters['name']
             self.get_logger().info('@@@@@@@@@@@@@@@@@@@@@@@@@@@ run_drcf')
-            os.system(command)
+            self.virtual_drcf = VirtualDRCF(port, model, name)
 
 def main(args=None):
     rclpy.init(args=args)
     node = ConnectionNode()
-    rclpy.spin_once(node)
+    rclpy.spin(node)
     rclpy.shutdown()
-
 
 if __name__ == '__main__':
     main()
